5661b9de3f8fcb8c3363f0c8cd576fcac560074b
Roman Kapitonov <roman.kapitonov@freshly.com>
2019-11-25 18:21:49 +0300

    Enables active model and active job extensions only if corresponding options are set in config.

--- lib/stenotype/railtie.rb
+++ lib/stenotype/railtie.rb
@@ -8,28 +8,26 @@ module Stenotype
   #
   class Railtie < ::Rails::Railtie
     Stenotype.configure do |c|
-      config.enable_action_controller_extension = true
-      config.enable_active_job_extension = true
+      c.enable_action_controller_extension = true
+      c.enable_active_job_extension = true
     end
 
     config.stenotype = Stenotype.config
 
-    #
-    # Register Rails handlers
-    #
-    Stenotype::ContextHandlers.module_eval do
-      register Stenotype::ContextHandlers::Rails::Controller
-      register Stenotype::ContextHandlers::Rails::ActiveJob
-    end
-
-    ActiveSupport.on_load(:action_controller) do
-      include Stenotype::Frameworks::Rails::ActionControllerExtension
+    if config.stenotype.enable_action_controller_extension
+      ActiveSupport.on_load(:action_controller) do
+        Stenotype::ContextHandlers.register Stenotype::ContextHandlers::Rails::Controller
+        include Stenotype::Frameworks::Rails::ActionControllerExtension
+      end
     end
 
     # @todo: consider using `::ActiveJob::Base.around_perform`
     #        or `::ActiveJob::Base.around_enqueue`
-    ActiveSupport.on_load(:active_job) do
-      extend Stenotype::Frameworks::Rails::ActiveJobExtension
+    if config.stenotype.enable_active_job_extension
+      ActiveSupport.on_load(:active_job) do
+        Stenotype::ContextHandlers.register Stenotype::ContextHandlers::Rails::ActiveJob
+        extend Stenotype::Frameworks::Rails::ActiveJobExtension
+      end
     end
   end
 end
